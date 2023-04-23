02e657ebff655b5df9fc88e883b19a8ea3a4081c
pranshu <srivastavapranshu2@gmail.com>
2023-04-24 00:15:55 +0530

    coloumns are now protected

--- app/Models/Likes.php
+++ app/Models/Likes.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Likes extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'count',
+        'post_id',
+        'user_id',
+        'category_id',
+    ];
 }
