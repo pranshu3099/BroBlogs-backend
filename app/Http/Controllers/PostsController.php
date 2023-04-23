9e3bff80cd1516956c15fb04094925be5c17b6da
pranshu <srivastavapranshu2@gmail.com>
2023-04-24 00:14:35 +0530

    added new function getHomePosts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -16,7 +16,18 @@ class PostsController extends Controller
             'content' => $request->content,
             'user_id' => $request->user_id,
             'category_id' => $request->category_id,
+            'post_id' => $request->post_id,
         ]);
         return response()->json(['success' => 'Post created Successfully'], 200);
+    }
+
+    public function getHomePost()
+    {
+        $results = Posts::with('user', 'category')
+            ->where('post_id', 1400)
+            ->first()
+            ->toArray();
+
+        return response()->json($results, 200);
     }
 }
