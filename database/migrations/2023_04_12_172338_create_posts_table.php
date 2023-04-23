f1f75804f1758cee91a09576cdc26a1590833d56
pranshu <srivastavapranshu2@gmail.com>
2023-04-24 00:17:51 +0530

    added foreign relation between posts and likes

--- database/migrations/2023_04_12_172338_create_posts_table.php
+++ database/migrations/2023_04_12_172338_create_posts_table.php
@@ -23,7 +23,7 @@ class CreatePostsTable extends Migration
         });
 
         Schema::table('likes', function (Blueprint $table) {
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('post_id')->on('posts');
         });
     }
 
