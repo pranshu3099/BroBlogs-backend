c7bc02224a413de90ccc5b5e9f1b996dd5e74c1c
pranshu <srivastavapranshu2@gmail.com>
2023-04-24 00:16:26 +0530

    added relationship for post model

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Posts extends Model
 {
@@ -14,5 +15,26 @@ class Posts extends Model
         'content',
         'user_id',
         'category_id',
+        'post_id',
     ];
+
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Likes::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Categories::class, 'category_id', 'slug');
+    }
 }
