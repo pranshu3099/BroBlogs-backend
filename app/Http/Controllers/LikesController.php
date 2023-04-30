ffabda2349dde749ecb4a4afd7e3e00d2039ad49
pranshu <srivastavapranshu2@gmail.com>
2023-04-30 23:51:17 +0530

    added like and remove method

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -3,9 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Likes;
 
 class LikesController extends Controller
 {
     //
-
+    public function AddremoveLikes(Request $request)
+    {
+        $post = Likes::where('post_id', $request->post_id)->first();
+        if (!($post->count == NULL || $post->count == 0)) {
+            $user_id_array = json_decode($post->user_id);
+            $post_user_id_array = array($request->user_id);
+            $user_id = [];
+            if ($request->likes > $post->count) {
+                $user_id = json_encode(array_merge($user_id_array, $post_user_id_array));
+            } else {
+                $user_id = array_diff($user_id_array, array($request->user_id));
+            }
+            Likes::where('post_id', $request->post_id)->update(['count' => $request->likes, 'user_id' => $user_id]);
+        } else {
+            $user_id_array = array($request->user_id);
+            $user_id = json_encode($post->user_id);
+            Likes::where('post_id', $request->post_id)->update(['count' => $request->likes, 'user_id' => $user_id_array]);
+        }
+        return response()->json(['likes' => $request->likes], 200);
+    }
 }
