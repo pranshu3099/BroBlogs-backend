a3bf31fc204aead8108ad2f9e7398e90a6b4ebf6
pranshu <srivastavapranshu2@gmail.com>
2023-05-20 23:03:41 +0530

    Changed create and get comments api

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -18,40 +18,27 @@ class CommentController extends Controller
     public function create(Request $request)
     {
         $comment = Comment::where('posts_id', $request->post_id)->first();
-        $user_id = array($request->user_id);
-        $user_comment = array($request->comment);
-        if (!$comment) {
-            Comment::create([
-                'posts_id' => $request->post_id,
-                'user_id' => json_encode($user_id),
-                'comment' => json_encode($user_comment),
-                'likes' => $request->likes,
-            ]);
-        } else {
-            $userId_array = json_decode($comment->user_id);
-            $userId_array = array_merge($userId_array, $user_id);
-            $comment_array = json_decode($comment->comment);
-            $comment_array = array_merge($comment_array, $user_comment);
-            Comment::where('posts_id', $request->post_id)->update(['comment' => json_encode($comment_array), 'user_id' => json_encode($userId_array)]);
-            return response()->json('success', 200);
-        }
+        $user_id = $request->user_id;
+        $user_comment = $request->comment;
+        Comment::create([
+            'posts_id' => $request->post_id,
+            'user_id' => $user_id,
+            'comment' => $user_comment,
+            'likes' => $request->likes,
+        ]);
+        return response()->json('success', 200);
     }
 
     public function getComments(Request $request, $id)
     {
         try {
-            $comments = Comment::where('posts_id', $id)->first();
-            $index = 0;
-            $user_comment = json_decode($comments->comment);
-            $results = DB::table('users')->join('comments', function ($join) use ($comments) {
-                $join->whereIn('users.id', json_decode($comments->user_id));
-            })->select('users.name')->get()->toArray();
-            $results =  array_map(function ($item) use ($user_comment, &$index) {
-                $item->comment = $user_comment[$index];
-                $index++;
-                return $item;
-            }, $results);
-            return response()->json(['comments' => $results], 200);
+            $results = DB::table('users')
+                ->join('comments', 'users.id', '=', 'comments.user_id')
+                ->where('comments.posts_id', $id)
+                ->select('users.name', 'comments.comment')
+                ->get()
+                ->toArray();
+            return response()->json($results, 200);
         } catch (\Exception $e) {
             Log::error($e->getMessage());
         }
